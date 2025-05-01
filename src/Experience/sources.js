a5b532a8395c2df16d1236abaa886162f4620541
Atharva Dhurwey <atharva.dhurwey.2000@gmail.com>
2025-05-01 16:20:49 +0530

    :fire: remove unused textures

--- src/Experience/sources.js
+++ src/Experience/sources.js
@@ -1,28 +1,5 @@
 export default [
   {
-    name: "environmentMapTexture",
-    type: "cubeTexture",
-    path: [
-      "textures/environmentMap/px.jpg",
-      "textures/environmentMap/nx.jpg",
-      "textures/environmentMap/py.jpg",
-      "textures/environmentMap/ny.jpg",
-      "textures/environmentMap/pz.jpg",
-      "textures/environmentMap/nz.jpg",
-    ],
-  },
-  {
-    name: "grassColorTexture",
-    type: "texture",
-    path: "textures/dirt/color.jpg",
-  },
-  {
-    name: "grassNormalTexture",
-    type: "texture",
-    path: "textures/dirt/normal.jpg",
-  },
-
-  {
     name: "Stone_Baked",
     type: "texture",
     path: "models/Skyisland/textures/Stone_Light_Baked.webp",
